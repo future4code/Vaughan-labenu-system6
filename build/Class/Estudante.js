89802b9ec2d95b6817b660092850d546f7e64815
MaiaJrDev <88292112+MaiaJrDev@users.noreply.github.com>
2022-04-25 09:43:12 -0300

    Delete build directory

--- build/Class/Estudante.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Estudante = void 0;
-const Usuario_1 = require("./Usuario");
-class Estudante extends Usuario_1.Usuario {
-}
-exports.Estudante = Estudante;
-//# sourceMappingURL=Estudante.js.map
